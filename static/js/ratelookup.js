b21fef27376873c29450c325b24f6e3e5f12e10e
Michael Shafrir <mshafrir@gmail.com>
2010-08-19 22:22:06 -0400

    Remove old autocomplete code, use new one.  Add date picker code.  Use form context in Jquery selector.

--- static/js/ratelookup.js
+++ static/js/ratelookup.js
@@ -1,70 +1,39 @@
 $(document).ready(function() {
-	console.log(all_hotels.length);
-	$("input#field_hotel").autocomplete(all_hotels, {
-			minChars: 0,
-			width: 200,
-			autoFill: false,
-			mustMatch: false,
-			dataType: 'json',
-			scroll: true,
-			scrollHeight: 300,
-			parse: function(data) {
-				var results = [];
-				$.each(data['results'], function(i) {
-					results.push({'data': this, 'value': this['name'], 'result': this['name']});
-				});
-				return results;
-			},
-			formatItem: function(item) {
-				return "<span class=\"result_name\"><span class=\"bold\">" + item.name + "</span></span>" + 
-						"<br /><span class=\"result_extra\">" + item.city + ", " + item.country + 
-						" - Category " + item.category + "</span>";
-				/*
-				var location = null;
-				if(item['city'] && item['city'].length > 1) {
-					location = item['city'] + ", " + item['state']
-				}
-				else {
-					location = item['state'];
-				}
+	var ratelookup_form = $("form#form_ratecode");
 	
-				return "<span class=\"result_name\">" + item['name'] + "</span>"
-						+ "<span class=\"result_extra\">" + location + "</span>";
-				*/
-			}
-		}
-	);
-
-	/*
-	//var hotels_source = "/services/autocomplete/hotels.json";
-	var hotels_source = all_hotels;
-	
-	$("input#field_hotel").autocomplete({
-		source: hotels_source,
-		minLength: 3,
-		//delay: 2000,
+	var hotel_field = $("input#field_hotel", ratelookup_form);
+	hotel_field.autocomplete({
+		source: all_hotels,
+		minLength: 1,
 		select: function(event, ui) {
-			$('input#field_hotel').val(ui.item.name);
-			$('input#field_hotel_id').val(ui.item.id);
+			hotel_field.val(ui.item.label);
+			$("input#field_hotel_id", ratelookup_form).val(ui.item.value);
 
 			return false;
 		}
 	})
 	.data("autocomplete")._renderItem = function(ul, item) {
+		var result_link = $("<a/>").append(
+							"<span class=\"bold\">" + item.label + "</span>"
+							+ "<br />" + item.desc);
 		return $("<li/>")
 			.data("item.autocomplete", item)
-			.append(
-				$("<a/>").append("<span class=\"bold\">" + item.name + "</span>" + 
-					"<br />" + item.city + ", " + item.country + 
-					" - Category " + item.category))
+			.append(result_link)
 			.appendTo(ul);
 	};
-	*/
 	
 	
-	$("span.ratecode_example").click(
+	$("input#field_date", ratelookup_form).datepicker({
+		dateFormat: "yy-mm-dd",
+		showOn: 'button',
+		buttonImage: '/static2/images/calendar.gif',
+		buttonImageOnly: true
+	});
+	
+	
+	$("span.ratecode_example", ratelookup_form).click(
 		function() {
-			$("input#field_ratecode").val($(this).text());
+			$("input#field_ratecode", ratelookup_form).val($(this).text());
 		}
 	);
 });
