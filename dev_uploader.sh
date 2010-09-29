fffd8b29ba29387f3b48aad6f97f35313b57688b
Michael Shafrir <mshafrir@gmail.com>
2010-09-29 16:46:46 -0400

    Support for StarwoodSetCode as well as StarwoodProperty

--- dev_uploader.sh
+++ dev_uploader.sh
@@ -1,2 +1,5 @@
+rm bulkloader-*.sql3
 rm bulkloader-*
-appcfg.py upload_data --config_file=bulkloader.yaml --filename=awardpad.csv --kind=StarwoodProperty --url=http://localhost:8102/remote_api .
+
+appcfg.py upload_data --config_file=bulkloader.yaml --filename=awardpad_starwoodproperty.csv --kind=StarwoodProperty --url=http://localhost:8102/remote_api .
+appcfg.py upload_data --config_file=bulkloader.yaml --filename=awardpad_starwoodsetcode.csv --kind=StarwoodSetCode --url=http://localhost:8102/remote_api .
