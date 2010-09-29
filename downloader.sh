1c8d94df9eca82005a05d0bb7d5d3cc588280485
Michael Shafrir <mshafrir@gmail.com>
2010-09-29 16:43:40 -0400

    Expand downloader script to support StarwoodSetCdoe as well as StarwoodProperty.

--- downloader.sh
+++ downloader.sh
@@ -1,1 +1,2 @@
-appcfg.py download_data --config_file=bulkloader.yaml --filename=awardpad.csv --kind=StarwoodProperty --url=http://awardr.appspot.com/remote_api
+appcfg.py download_data --config_file=bulkloader.yaml --filename=awardpad_starwoodproperty.csv --kind=StarwoodProperty --url=http://awardr.appspot.com/remote_api
+appcfg.py download_data --config_file=bulkloader.yaml --filename=awardpad_starwoodsetcode.csv --kind=StarwoodSetCode --url=http://awardr.appspot.com/remote_api
