2686253610740cf68e9fb7045ee7f77e68e18a21
Michael Shafrir <mshafrir@gmail.com>
2011-01-07 18:50:12 -0500

    - Fixed session cookie method by changing https:// -> http://

---
+++ app/helper_starwood.py
@@ -0,0 +1,8 @@
+from google.appengine.api import urlfetch
+
+import logging
+logging.getLogger().setLevel(logging.DEBUG)
+
+def get_session_cookie():
+	response = urlfetch.fetch('http://www.starwoodhotels.com/preferredguest/account/sign_in.html?login=%s&password=%s' % ('awardpad', 'Jordan23'))
+	return {'Cookie': "JSESSIONID=%s" % response.headers.get('set-cookie').split('JSESSIONID=')[1].split(';')[0]}
