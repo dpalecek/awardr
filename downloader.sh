f371b68d908b4e2da7bc9b9fd0c3e0cfad34ab4a
Michael Shafrir <mshafrir@gmail.com>
2011-02-28 22:04:29 -0500

    - Delete old sqlite and csv files before downloading.

--- downloader.sh
+++ downloader.sh
@@ -1,2 +1,4 @@
+rm awardpad.csv awardpad_starwoodproperty.csv *.sql3
+
 appcfg.py download_data --config_file=bulkloader.yaml --filename=awardpad_starwoodproperty.csv --kind=StarwoodProperty --url=http://awardr.appspot.com/remote_api
-appcfg.py download_data --config_file=bulkloader.yaml --filename=awardpad_starwoodsetcode.csv --kind=StarwoodSetCode --url=http://awardr.appspot.com/remote_api
+#appcfg.py download_data --config_file=bulkloader.yaml --filename=awardpad_starwoodsetcode.csv --kind=StarwoodSetCode --url=http://awardr.appspot.com/remote_api
