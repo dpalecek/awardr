9068a2d6d97072cd092ffa175f66ed13e4a5d2cf
Michael Shafrir <mshafrir@gmail.com>
2011-02-28 23:21:04 -0500

    - Update downloader.

--- downloader.sh
+++ downloader.sh
@@ -1,4 +1,5 @@
 rm awardpad.csv awardpad_starwoodproperty.csv *.sql3
+rm bulkloader-*.sql3 bulkloader-log-*.*
 
-appcfg.py download_data --config_file=bulkloader.yaml --filename=awardpad_starwoodproperty.csv --kind=StarwoodProperty --url=http://awardr.appspot.com/remote_api
+appcfg.py download_data --config_file=bulkloader2.yaml --filename=awardpad_starwoodproperty.csv --kind=StarwoodProperty --url=http://awardr.appspot.com/remote_api
 #appcfg.py download_data --config_file=bulkloader.yaml --filename=awardpad_starwoodsetcode.csv --kind=StarwoodSetCode --url=http://awardr.appspot.com/remote_api
