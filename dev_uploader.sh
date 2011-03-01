10e125d4a331a14f878168046ce37901a1093643
Michael Shafrir <mshafrir@gmail.com>
2011-02-28 23:21:12 -0500

    Update uploader.

--- dev_uploader.sh
+++ dev_uploader.sh
@@ -1,5 +1,4 @@
-rm bulkloader-*.sql3
-rm bulkloader-*
+rm bulkloader-*.sql3 bulkloader-log-*.*
 
-appcfg.py upload_data --config_file=bulkloader.yaml --filename=awardpad_starwoodproperty.csv --kind=StarwoodProperty --url=http://localhost:8102/remote_api .
-appcfg.py upload_data --config_file=bulkloader.yaml --filename=awardpad_starwoodsetcode.csv --kind=StarwoodSetCode --url=http://localhost:8102/remote_api .
+appcfg.py upload_data --num_threads=4 --config_file=bulkloader.yaml --filename=awardpad_starwoodproperty.csv --kind=StarwoodProperty --url=http://localhost:8102/remote_api .
+#appcfg.py upload_data --config_file=bulkloader.yaml --filename=awardpad_starwoodsetcode.csv --kind=StarwoodSetCode --url=http://localhost:8102/remote_api .
