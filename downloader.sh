6f6609d8083676637237dfb7a71c0c1e41335797
Michael Shafrir <mshafrir@gmail.com>
2011-03-04 00:21:34 -0500

    - Consolidate rm lines.

--- downloader.sh
+++ downloader.sh
@@ -1,5 +1,4 @@
-rm awardpad.csv awardpad_starwoodproperty.csv *.sql3
-rm bulkloader-*.sql3 bulkloader-log-*.*
+rm awardpad.csv awardpad_starwoodproperty.csv *.sql3 bulkloader-*.sql3 bulkloader-log-*.*
 
 appcfg.py download_data --config_file=bulkloader2.yaml --filename=awardpad_starwoodproperty.csv --kind=StarwoodProperty --url=http://awardr.appspot.com/remote_api
 #appcfg.py download_data --config_file=bulkloader.yaml --filename=awardpad_starwoodsetcode.csv --kind=StarwoodSetCode --url=http://awardr.appspot.com/remote_api
